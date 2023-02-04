78df1a5771fad0ab66db5be97195c6b1796880ed
al428145 <al428145@uji.es>
2023-02-04 16:05:28 +0100

    logica de pelea y fuente

---
+++ WarDecision/Assets/Scripts/Logica de pelea.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logicadepelea : MonoBehaviour
+{
+    public int gold;
+    public int troops;
+    public int player1Troops;
+    public int player2Troops;
+
+    public void SendTroops(int troopsToSend, int player)
+    {
+        // Verificar si hay suficiente oro para enviar soldados
+        float cost = 0.25f * troopsToSend;
+        if (gold < cost)
+        {
+            Debug.Log("No hay suficiente oro para enviar soldados");
+            return;
+        }
+
+        // Restar oro
+        gold -= (int)cost;
+
+        // Generar número aleatorio
+        int randomValue = Random.Range(1, 7);
+
+        // Calcular el resultado de la batalla
+        int battleResult = randomValue * troopsToSend;
+
+        // Guardar el resultado de la batalla según el jugador
+        if (player == 1)
+        {
+            player1Troops += battleResult;
+        }
+        else if (player == 2)
+        {
+            player2Troops += battleResult;
+        }
+
+        // Calcular la diferencia de tropas
+        int troopsDifference = Mathf.Abs(player1Troops - player2Troops);
+
+        // Decidir quién enviará más soldados
+        int winner;
+        if (player1Troops > player2Troops)
+        {
+            winner = 1;
+        }
+        else
+        {
+            winner = 2;
+        }
+
+        // Aplicar las pérdidas de tropas según la diferencia de tropas
+        if (troopsDifference > 10000)
+        {
+            if (winner == 1)
+            {
+                player1Troops -= (int)(0.2f * battleResult);
+                player2Troops -= (int)(0.75f * battleResult);
+            }
+            else
+            {
+                player1Troops -= (int)(0.75f * battleResult);
+                player2Troops -= (int)(0.2f * battleResult);
+            }
+        }
+        else
+        {
+            if (winner == 1)
+            {
+                player1Troops -= (int)(0.4f * battleResult);
+                player2Troops -= (int)(0.5f * battleResult);
+            }
+            else
+            {
+                player1Troops -= (int)(0.5f * battleResult);
+                player2Troops -= (int)(0.4f * battleResult);
+            }
+        }
+
+        // Añadir oro al ganador
+        if (winner == 1)
+        {
+            gold += 200;
+        }
+        else
+        {
+            gold += 200;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
