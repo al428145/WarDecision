053e15cb381abf91b63deb6ff2220832d6e07d46
WarGodMars <skull_thegamer@abv.bg>
2023-02-05 10:52:09 +0100

    ScriptsDificultad

---
+++ WarDecision/Assets/Scripts/ControllerDificulty.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerDificulty : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Dificultad;
+}
